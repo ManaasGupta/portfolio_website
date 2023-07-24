81d960a16cceff98dcdbd48ecf817535623abfb1
ManaasGupta <manasgpt3@gmail.com>
2023-07-24 21:24:06 +0530

    modified database.py

--- database.py
+++ database.py
@@ -5,30 +5,29 @@ import os
 import hashlib
 def get_data():
     lh_host=os.getenv("LOCAL_HOST")
-    lh_password=os.getenv("LOCAL_USER")
+    lh_password=os.getenv("LOCAL_PASSWORD")
     lh_database=os.getenv("LOCAL_DATABASE")
     lh_port=os.getenv("LOCAL_PORT")
     lh_user=os.getenv("LOCAL_USER")
     eng_string=f"mysql+mysqlconnector://{lh_user}:{lh_password}@{lh_host}:{lh_port}/{lh_database}"
     engine=create_engine(eng_string,echo=True)
     with engine.connect() as conn:
-        conn.execute(text("use query;"))
-        login_details=conn.execute(text("SELECT * FROM valid_users;"))
-        login_details_dict=login_details.mappings().all()
-        return login_details_dict
+        conn.execute(text(""" CREATE TABLE valid_users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    hashed_password VARCHAR(200) NOT NULL,
+	date_of_creation DATE,
+    day INT,
+    month INT,
+    year INT, -- Missing comma here
+    month_name CHAR(30) -- Missing comma was added here
+);"""))
+        conn.commit()
+        conn.close()
         
 if __name__=="__main__":
     #Example SQL Query to fetch data from table 'users' where user id is 12
     # sql_string=str(input("Enter SQL Query: "))
-    res=get_data()
-    for row in res:
-        print(row['email'])
-        print(row['hashed_password'])
-    email=str(input("Enter Email"))
-    password=str(input("Enter Password"))
-    for row in res:
-        if (row["email"]==email) and (row['hashed_password']==hashlib.md5(password.encode()).hexdigest()):
-            print("Login Sucessfull")
-            break
-    else:
-        print("Invalid Credentials")
+    get_data()
